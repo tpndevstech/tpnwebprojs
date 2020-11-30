5f7ab327190bc3fa7c2b77440e127138ff25d6d3
Tapan Anand <70485285+tapananand95@users.noreply.github.com>
2020-11-30 20:29:01 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const navbar = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("#sidebar");
+const date = document.querySelector("#date");
+// add fixed class to navbar
+
+// show sidebar
+navBtn.addEventListener("click", function () {
+  sidebar.classList.add("show-sidebar");
+});
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+// set year
+date.innerHTML = new Date().getFullYear();
